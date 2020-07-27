23c7a3745e9786907f91ac67a21388d36d10aa4a
nassim <nas897700@gmail.com>
2020-07-27 14:29:11 +0100

    categories

--- lib/views/Categories/materiel.dart
+++ lib/views/Categories/materiel.dart
@@ -1,3 +1,6 @@
+import 'package:bingo/constant/ProductCard.dart';
+import 'package:bingo/model/Product.dart';
+import 'package:bingo/model/User.dart';
 import 'package:flutter/material.dart';
 
 class Material extends StatefulWidget {
@@ -6,8 +9,152 @@ class Material extends StatefulWidget {
 }
 
 class _MaterialState extends State<Material> {
+  List<Product> list = [
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alge r,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Electronique"),
+        centerTitle: true,
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, childAspectRatio: 0.6),
+        scrollDirection: Axis.vertical,
+        itemCount: list.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ProductCard(
+            product: list[index],
+          );
+        },
+      ),
+    );
   }
 }
--- lib/views/Categories/telephone.dart
+++ lib/views/Categories/telephone.dart
@@ -1,3 +1,6 @@
+import 'package:bingo/constant/ProductCard.dart';
+import 'package:bingo/model/Product.dart';
+import 'package:bingo/model/User.dart';
 import 'package:flutter/material.dart';
 
 class Telephone extends StatefulWidget {
@@ -6,12 +9,152 @@ class Telephone extends StatefulWidget {
 }
 
 class _TelephoneState extends State<Telephone> {
+  List<Product> list = [
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alger,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+    Product(
+        categorie: '',
+        productPrice: 1500,
+        productEtat: 'Neuf',
+        producteName: 'acer-5468',
+        producteDescription:
+            "Google LLC /ˈguːgəl/ est une entreprise américaine de services technologiques fondée en 1998 dans la Silicon Valley, en Californie, par Larry Page et Sergey Brin, créateurs du moteur de recherche Google. C'est une filiale de la société Alphabet depuis août 2015.",
+        producteUrlPic1: 'http://via.placeholder.com/413x275',
+        producteUrlPic2: 'http://via.placeholder.com/413x275',
+        producteUrlPic3: 'http://via.placeholder.com/413x275',
+        user: User(
+            location: 'Alge r,oran',
+            name: 'Abrous Nassim',
+            selNumber: '0557059399')),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Téléphones"),
+        centerTitle: true,
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, childAspectRatio: 0.6),
+        scrollDirection: Axis.vertical,
+        itemCount: list.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ProductCard(
+            product: list[index],
+          );
+        },
+      ),
+    );
   }
 }
-/*
-
-lLmo§!mkadkmjmljmsmqdkmkfsmqlkjjmlsqjkfm!!kjfmsdjgfmhjhgKHMLHGMHFDBVMKJEFHB
-*/
